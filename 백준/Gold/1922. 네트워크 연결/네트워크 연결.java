6642fe4254efbd4779f3ccbe5d5c92f866253d82
이지우 <74723818+CordHouse@users.noreply.github.com>
2023-06-09 15:38:24 +0900

    [Gold IV] Title: 네트워크 연결, Time: 636 ms, Memory: 56956 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * 주어진 조건
@@ -14,7 +12,14 @@ import java.util.List;
  * 1 <= N <= 1_000
  * 1 <= M <= 100_000
  * a와 b는 같을 수도 있다.
- */
+ *
+ * 풀이
+ * MST -> 최소 신장 트리
+ * 크루스칼 알고리즘 사용 -> O(ElogE) -> 간선의 수가 적은 경우 유리
+ * 프림 알고리즘 -> O(n^2) -> 간선의 수가 많은 경우 유리
+ * 성능 개선 전 List 풀이
+ * 성능 개선 후 PriorityQueue 풀이
+ * */
 public class Main {
     private static int[] parent;
     public static void main(String[] args) throws IOException {
@@ -28,24 +33,21 @@ public class Main {
         for(int i = 1; i <= n; i++) { // 부모 배열 초기화
             parent[i] = i;
         }
-
+        
         // 노드에 컴퓨터간의 간선정보 집어넣기
-        List<Node> nodeList = new ArrayList<>();
+        PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.value - o2.value);
         for(int i = 0; i < m; i++) {
             String[] line = br.readLine().split(" ");
             int s = Integer.parseInt(line[0]);
             int e = Integer.parseInt(line[1]);
             int v = Integer.parseInt(line[2]);
-            nodeList.add(new Node(s, e, v));
+            queue.add(new Node(s, e, v));
         }
 
-        // 정렬하기
-        Collections.sort(nodeList, (o1, o2) -> o1.value - o2.value);
-
         // 컴퓨터 사이를 확인하고 부모 배열에 대해 값을 바꿔 사이클되지 않도록 설정한다.
-        for(int i = 0; i < m; i++) {
-            Node node = nodeList.get(i);
-            if(find(node.start) != find(node.end)) { // 두 부모가 같은지 먼저 확인 -> 오름차순 정렬 했기 때문에 가능
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            if(find(node.start) != find(node.end)) { // 두 부모가 같은지 먼저 확인
                 answer += node.value;
                 union(node.start, node.end); // 부모 병합과정 시작 -> 부모를 같게 만들어 줌으로서 순환구조를 생성하지 않음
             }
