4de862e674b3d1b15f54402290470c9f9d702a4b
이지우 <74723818+CordHouse@users.noreply.github.com>
2023-06-17 17:20:33 +0900

    [Gold V] Title: ABCDE, Time: 332 ms, Memory: 20856 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static int n, m;
+    private static int answer = 0;
+    private static List<Integer>[] node;
+    private static boolean[] visit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        node = new ArrayList[n];
+
+        for(int i = 0; i < n; i++) {
+            node[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < m; i++) {
+            String[] line = br.readLine().split(" ");
+            int left = Integer.parseInt(line[0]);
+            int right = Integer.parseInt(line[1]);
+            node[left].add(right);
+            node[right].add(left);
+        }
+
+        visit = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(answer == 0) {
+                dfs(1, i);
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void dfs(int depth, int start) {
+        if(depth == 5) {
+            answer = 1;
+            return;
+        }
+
+        visit[start] = true;
+        for(int v : node[start]) {
+            if(!visit[v]) {
+                dfs(depth+1, v);
+            }
+        }
+        visit[start] = false;
+    }
+}
