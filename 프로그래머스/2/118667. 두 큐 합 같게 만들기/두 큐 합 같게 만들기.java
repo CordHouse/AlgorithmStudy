230c81fc364402e1aa1fcbee9304ea0faf81b518
이지우 <74723818+CordHouse@users.noreply.github.com>
2023-12-01 03:13:17 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 11.53 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;        
+        int[] mixQueue = new int[queue1.length + queue2.length];
+        
+        for(int i = 0; i < queue1.length; i++) {
+            mixQueue[i] = queue1[i];
+            mixQueue[i+queue1.length] = queue2[i];
+        }
+        
+        long total = sum(mixQueue);     
+        if(total % 2 == 1) {
+            return -1;
+        }
+        
+        int left = 0, right = queue1.length - 1;
+        long s1 = sum(queue1);
+        long s2 = sum(queue2);
+        long save = s1;
+        while(s1 != s2) {
+            if(s1 == 0 || s2 == 0) {
+                return -1;
+            }
+            if(s1 < s2) {
+                right++;
+                s1 += mixQueue[right % mixQueue.length];
+                s2 -= mixQueue[right % mixQueue.length];
+            }
+            else if(s1 > s2) {
+                s1 -= mixQueue[left % mixQueue.length];
+                s2 += mixQueue[left % mixQueue.length];
+                left++;
+            }
+            if(left % mixQueue.length == 0 && right % mixQueue.length == queue1.length - 1) return -1;
+            if(left > mixQueue.length && right > mixQueue.length) return -1;
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+    
+    public int sum(int[] arr) {
+        int sum = 0;
+        for(int v : arr) {
+            sum += v;
+        }
+        return sum;
+    }
+}
